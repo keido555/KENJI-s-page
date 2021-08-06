bea6345e84471ce78ff499081d8bc2af65de06fa
keido <w03optfd73@gmail.com>
2021-08-06 15:22:16 +0900

    js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,17 @@ $(function(){
     });
 });
 
+$(function() {
+    $('a[href^="#"]').click(function() {
+        let speed = 400;
+        let href = $(this).attr("href");
+        let target = $(href == "#" || href == "" ? 'html' : href);
+        let position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    })
+})
+
 // home > btn
 // ==================================================
 $(function() {
